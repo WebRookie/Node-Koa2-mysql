7d3ee40f88748ba2683aa8f9225b4297a25d441a
WebRookie <912962951@qq.com>
2021-07-01 15:12:24 +0800

    日志输出格式修改

--- start/backEnd/myBlogBackEnd/config/logger_config.js
+++ start/backEnd/myBlogBackEnd/config/logger_config.js
@@ -15,7 +15,7 @@ module.exports = {
             type: 'dateFile',                 //logger类型
             filename: errorLogPath,           //日志输出位置
             alwaysIncludePattern: true,       //是否总是有后缀名
-            pattern: '-yyyy-MM-dd-hh.log',     //后缀。每小时创建一个新的日志
+            pattern: '-yyyy-MM-dd.log',     //后缀。每天创建一个新的日志
             level:'ERROR'
         },
         //响应日志
@@ -24,7 +24,7 @@ module.exports = {
             type:'dateFile',
             filename: responseLogPath,
             alwaysIncludePattern: true,
-            pattern: '-yyyy-MM-dd-hh.log',
+            pattern: '-yyyy-MM-dd.log',
             level: 'ALL'
         }
     },
